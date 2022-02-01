eb3abd9eea6dbe5eaebd5ba90b9f55f0f8254905
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-02-01 22:50:54 +0530

    Add project files.

---
+++ PatternCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_24_UserRegistration
+{
+    internal class PatternCheck
+    {
+
+        //pattern for valid name
+        public static string REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        public bool FirstName(string name)
+        {
+            return Regex.IsMatch(name, REGEX);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day_24_UserRegistration
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Reg using Lambda Expressions!");
+            Console.WriteLine("Enter First Name :");
+            string name = Console.ReadLine();
+            PatternCheck patternCheck = new PatternCheck();
+            if (patternCheck.FirstName(name))
+                Console.WriteLine(" First Name Is Valid");
+            else
+                Console.WriteLine("First Name Is Invalid");
+        }
+    }
+}
